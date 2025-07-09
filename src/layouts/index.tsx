abff362de6dd4f2caeba95938e104953814f4069
mkcmgl <2515318247@qq.com>
2025-07-09 11:25:06 +0800

    feat:title

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,17 +1,40 @@
 import { Divider, Layout, theme } from 'antd';
 import React from 'react';
-import { Outlet } from 'umi';
+import { Outlet, useLocation } from 'umi';
 import '../locales/config';
 import Header from './components/header';
+import { useEffect } from 'react';
 
 import styles from './index.less';
 
 const { Content } = Layout;
 
+const mainMenuMap: Record<string, string> = {
+  '/knowledge': '知识库',
+  '/chat': '智能聊天',
+  '/file': '文件管理',
+  '/trial': '检索测试',
+  '/tools': '工具集',
+};
+
+const DEFAULT_TITLE = '政企RAG';
+
 const App: React.FC = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const location = useLocation();
+
+  useEffect(() => {
+    const matched = Object.entries(mainMenuMap).find(([path]) =>
+      location.pathname.startsWith(path)
+    );
+    if (matched) {
+      document.title = `${DEFAULT_TITLE}-${matched[1]}`;
+    } else {
+      document.title = DEFAULT_TITLE;
+    }
+  }, [location.pathname]);
 
   return (
     <Layout className={styles.layout}>
